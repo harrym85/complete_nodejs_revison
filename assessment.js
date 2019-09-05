fe6f99ef80843a9a2a4e756f377c13962d4774cf
thembhani mapengo <thembhanimapengo@thembhanis-MacBook-Pro.local>
2019-09-06 01:03:28 +0200

    chages

--- assessment.js
+++ assessment.js
@@ -7,16 +7,48 @@
 // }
 
 
-// A.1.2 Loops I
-let x = 0;
-for (k = 1; k < 5; k++){
-  x  = x + k
-    for (l = 1; l < 6; l++){
-     if(l > k){
-       x = x + l
-     }
-    }
-}
-console.log(x);
+// A.1.2 Loops II
+
+// let X = 0;
+// for (k = 1; k < 5; k++){
+//   X = X + k
+//     for (l = 1; l < 6; l++){
+//      if(l > k){
+//       X = X + l
+//      }
+//     }
+// }
+// console.log(`The value of variable X after this code executes is`,X);
+
+
+// A.1.3 Functions
+
+// const  doSomething = (x) => {
+//     if (x === 0) return 1; // base 
+//     console.log("do",doSomething(x - 1))
+//     return x * doSomething(x - 1);// recursion 
+//   }
+// console.log(`when we call doSomething(5) it then returns `,doSomething(5))
+
+//This is a recursive function which does the following the base case  stops the recursion and  the recursion is when the function calls itself 
+
+
+//A.1.4 Recursion 
+
+// result = 1 
+// const  doSomething1 = (x) => {
+//         while(x > 1 ){
+//             result *= x--
+//         }
+//         return result
+//       }
+// console.log(doSomething1(5))
+
+
+
+//A.1.5 Loops III
+
+
+
 
 
